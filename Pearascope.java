14f3f50d547e7c6143db22f6cddf6a5a2a86479a
Nazeer Hoque <theonlyoneth@outlook.com>
2024-04-03 18:39:37 -0500

    Add more data, cleanup code

--- Pearascope.java
+++ Pearascope.java
@@ -5,90 +5,74 @@ import com.opencsv.CSVWriter;
 
 public class Pearascope {
     public static void main(String[] args) {
+        String fileName = "../Log_24-03-23_14-23-19_q20.csv"; // <-- Change to use
+        long time = System.currentTimeMillis();
         try {
-            CSVReader r = new CSVReader(new FileReader("Log_24-03-23_17-54-44_q44.csv"));
+            CSVReader r = new CSVReader(new FileReader(fileName));
             String[] nextLine = r.readNext();
 
-            String[] columns = {
-                "/DriverStation/Matchnumber",
-                "/DriverStation/AllianceStation",
-                "/DriverStation/Autonomous", 
-                "/RealOutputs/Amp Bar/Amp Bar Position", 
-                "/DriverStation/MatchTime", 
-                "/RealOutputs/Transport/Ir Sensor", 
-                "/RealOutputs/Drivetrain/Odometry/rotation/value",
-                "/RealOutputs/Drivetrain/Odometry/translation/x", 
-                "/RealOutputs/Drivetrain/Odometry/translation/y",
-                "/RealOutputs/Shooter/NoteVelocity",
-                "/RealOutputs/Shooter/Shooter Pivot Position",
-                "/RealOutputs/Shooter/Shooter Pivot Intended Position",
-            };
-
-
+            // creates a hashmap of indexes of the logs' columns (which column has what)
             HashMap<String, Integer> columnIndexes = new HashMap<>();
-            for (int i = 0; i < nextLine.length; i++) {
-                for (String c : columns) {
-                    if (nextLine[i].equals(c)) {
-                        columnIndexes.put(c, i);
-                    }
-                }
-            }
-            // System.err.println(beamBreakIdx + " " + timestampIdx);
-            boolean hasNote = true; // preload
-            // System.out.print("preload\t");
+            for (int i = 0; i < nextLine.length; i++) { columnIndexes.put(nextLine[i], i); }
+            
+            // first line of output, headers of columns
+            String[] headers = { "Intook Time", "Intook Pose X", "Intook Pose Y", "Intook Pose θ", 
+                    "Shot Time", "Shot X", "Shot Y", "Shot θ", 
+                    "Autonomous/Teleop", "Speaker/Amp",
+                    "Pivot Intended Position", "Pivot Actual Position",
+                    "Left Shooter RPM", "Right Shooter RPM",
+                    "Limelight Ambiguity", "Battery Voltage",
+                    // "Match Number", "Alliance" 
+            };
 
             CSVWriter w = new CSVWriter(new FileWriter("Output.csv"));
-
-            String[] headers = { "time intook", "intook x", "intook y", "intook theta", 
-                "time shot", "shot x", "shot y", "shot theta", 
-                "autonomous", "speaker/amp" 
-                //"shooter rpm", "pivot position", 
-                // "match number", "isRedAlliance" 
-            };
-            w.writeNext(headers);
+            w.writeNext(headers); // writes headers to first row of output
             
             ArrayList<String> output = new ArrayList<>();
-            output.add("preload");
-            output.add("preload");
-            output.add("preload");
-            output.add("preload");
+            for (int i = 0; i < 4; i++) { output.add("PRELOAD"); }
 
-            while ((nextLine = r.readNext()) != null) {
+            boolean hasNote = true; // starts with preload
+
+            while ((nextLine = r.readNext()) != null) { // reads next line until it runs out of rows in logs
+                // if in match and whether the robot has a note changes
                 if (Integer.parseInt(nextLine[columnIndexes.get("/DriverStation/MatchTime")]) > 0) {
                     if (nextLine[columnIndexes.get("/RealOutputs/Transport/Ir Sensor")].equals("true") != hasNote) {
-                        hasNote = !hasNote;
-                        // System.out.print(hasNote ? "Intook @ " : "Shot @ ");
-                        // System.out.print(Integer.parseInt(nextLine[columnIndexes.get("/DriverStation/MatchTime")]));
-                        // System.out.print(hasNote ? "\t" : "\n");
-                        if (hasNote) {
-                            output.add(nextLine[columnIndexes.get("/DriverStation/MatchTime")]);
-                            output.add(nextLine[columnIndexes.get("/RealOutputs/Drivetrain/Odometry/translation/x")]);
-                            output.add(nextLine[columnIndexes.get("/RealOutputs/Drivetrain/Odometry/translation/y")]);
-                            output.add(nextLine[columnIndexes.get("/RealOutputs/Drivetrain/Odometry/rotation/value")]);
-                        } else {
-                            output.add(nextLine[columnIndexes.get("/DriverStation/MatchTime")]);
-                            output.add(nextLine[columnIndexes.get("/RealOutputs/Drivetrain/Odometry/translation/x")]);
-                            output.add(nextLine[columnIndexes.get("/RealOutputs/Drivetrain/Odometry/translation/y")]);
-                            output.add(nextLine[columnIndexes.get("/RealOutputs/Drivetrain/Odometry/rotation/value")]);
+                        hasNote = !hasNote; // updates local variable
 
-                            output.add(nextLine[columnIndexes.get("/DriverStation/Autonomous")]);
-                            output.add(Double.parseDouble(nextLine[
-                                columnIndexes.get("/RealOutputs/Amp Bar/Amp Bar Position")]) > 5 ? "true" : "false");
-                            // System.err.println(Double.parseDouble(nextLine[
-                            //     columnIndexes.get("/RealOutputs/Amp Bar/Amp Bar Position")]) > 5);
+                        // logs time and pose when robot intakes or shoots
+                        output.add(nextLine[columnIndexes.get("/DriverStation/MatchTime")]);
+                        output.add(nextLine[columnIndexes.get("/RealOutputs/Drivetrain/Odometry/translation/x")]);
+                        output.add(nextLine[columnIndexes.get("/RealOutputs/Drivetrain/Odometry/translation/y")]);
+                        output.add(nextLine[columnIndexes.get("/RealOutputs/Drivetrain/Odometry/rotation/value")]);
+                        
+                        if (!hasNote) { // logs these when the robot shoots and no longer has a note
+                            output.add(nextLine[columnIndexes.get("/DriverStation/Autonomous")]
+                            .equals("true") ? "Autonomous" : "Teleop");
+                            output.add(Double.parseDouble(nextLine[columnIndexes.get
+                                ("/RealOutputs/Amp Bar/Amp Bar Position")]) > 5 ? "Amp" : "Speaker");
+                            output.add(nextLine[columnIndexes.get("/RealOutputs/Shooter/Shooter Pivot Intended Position")]);
+                            output.add(nextLine[columnIndexes.get("/RealOutputs/Shooter/Shooter Pivot Position")]);
+                            output.add(nextLine[columnIndexes.get("/RealOutputs/Shooter/Shooter Left Speed")]);
+                            output.add(nextLine[columnIndexes.get("/RealOutputs/Shooter/Shooter Right Speed")]);
+                            output.add(nextLine[columnIndexes.get("/RealOutputs/Limelight/Single Tag Ambiguity")]);
+                            output.add(nextLine[columnIndexes.get("/SystemStats/BatteryVoltage")]);                            
+                            // output.add(nextLine[columnIndexes.get("/DriverStation/MatchNumber")]);
+                            // output.add(Integer.parseInt(nextLine[columnIndexes.get
+                            //     ("/DriverStation/AllianceStation/DriverStation")]) > 3 ? "Red" : "Blue");
 
+                            // converts output to String[] and writes it to output
                             w.writeNext(Arrays.copyOf(output.toArray(), output.size(), String[].class));
-                            output = new ArrayList<>();
+                            output = new ArrayList<>(); // clears variable for new line
                         }
                     }
                 }
-                // System.err.println(Integer.parseInt(nextLine[timestampIdx]));
             }
             
             r.close();
             w.close();
             
             System.out.println("Done! Check Output.csv");
+            System.out.println("Took " + ((System.currentTimeMillis() - time) / 1000.0) + "s");
         } catch (Exception e) {
             e.printStackTrace();
         }        
