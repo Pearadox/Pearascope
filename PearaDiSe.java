aec5af5cf0ab9cdd3e211278b37ecd396d18ea80
Nazeer Hoque <theonlyoneth@outlook.com>
2024-04-10 15:29:39 -0500

    Output PDH kJs used to output.csv

--- PearaDiSe.java
+++ PearaDiSe.java
@@ -14,37 +14,73 @@ public class PearaDiSe {
             // creates a hashmap of indexes of the logs' columns (which column has what)
             HashMap<String, Integer> columnIndexes = new HashMap<>();
             for (int i = 0; i < nextLine.length; i++) { columnIndexes.put(nextLine[i], i); }
-            
-            // first line of output, headers of columns
-            String[] headers = { "# of Brownouts" };
-            
-            // CSVWriter w = new CSVWriter(new FileWriter("../dcmp_output/" + trim(fileName)));
-            // w.writeNext(headers); // writes headers to first row of output
-            
-            ArrayList<String> output = new ArrayList<>();
 
-            boolean brownedOut = false; 
-            int brownOutCounter = 0;
-            int lineCounter = 0;
-            double totalPDP_sum = 0;
-
+            final double kConv = 0.00000556 * 3.6; // pls explain what these constants mean
+            int brownOutCounter = 0, lineCounter = 0;
+            double totalPDP_sum = 0, kJ_first_brownout = 0;            
+            double[] PDP_sums = new double[24];
+            
             while ((nextLine = r.readNext()) != null) { 
                 lineCounter++;
+
                 if (nextLine[columnIndexes.get("Brownout")].equals("True")) {
                     brownOutCounter++;
                 }
-                totalPDP_sum += Double.parseDouble(nextLine[columnIndexes.get("Total PDP")]) * Double.parseDouble(nextLine[columnIndexes.get("Voltage")]);
-            }
-            totalPDP_sum *= 0.00000556 * 3.6;
+                
+                double voltage = Double.parseDouble(nextLine[columnIndexes.get("Voltage")]);                
+                if (voltage < 30) {
+                    totalPDP_sum += Double.parseDouble(nextLine[columnIndexes.get("Total PDP")]) 
+                    * voltage
+                    * kConv;
+                    
+                    for (int i = 0; i <= 23; i++) {
+                        PDP_sums[i] += Double.parseDouble(nextLine[
+                            columnIndexes.get("PDP " + String.valueOf(i))]) 
+                            * voltage
+                            * kConv;
+                    }
+                }
 
+                if (brownOutCounter == 0) { 
+                    // updates until brownout counter > 0
+                    kJ_first_brownout = totalPDP_sum;
+                }
+            }
+
+            // first line of output, headers of columns
+            String[] headers = new String[27];
+            headers[0] = "Total kJ";
+            headers[1] = "# of Brownouts";
+            headers[2] = "kJ @ 1st BO";
+
+            for (int i = 0; i <= 23; i++) { 
+                headers[i + 3] = "PDP " + String.valueOf(i); 
+            }
+            
+            ArrayList<String> output = new ArrayList<>();
+            
+            output.add(String.valueOf(totalPDP_sum));
+            output.add(String.valueOf(brownOutCounter));
+            output.add(String.valueOf(kJ_first_brownout));
+            
+            for (int i = 0; i <= 23; i++) {
+                System.out.println("PDP " + i + ": " + PDP_sums[i] + " kJ");
+                output.add(String.valueOf(PDP_sums[i]));
+            }
+
+            CSVWriter w = new CSVWriter(new FileWriter("output.csv"));
+            w.writeNext(headers); // writes headers to first row of output
+            w.writeNext(Arrays.copyOf(output.toArray(), output.size(), String[].class));
+            
+            System.out.println("\nTotal PDP " + totalPDP_sum + " kJ");
+            System.out.println("kJ @ 1st BO " + kJ_first_brownout + " kJ");
             System.out.println("Found " + brownOutCounter + " brownouts");
-            System.out.println("Total PDP " + totalPDP_sum + " kJ");
+            
             System.out.println("Checked " + lineCounter + " lines");
+            System.out.println("Took " + ((System.currentTimeMillis() - time) / 1000.0) + "s");
             
             r.close();
-            // w.close();
-            
-            System.out.println("Took " + ((System.currentTimeMillis() - time) / 1000.0) + "s");
+            w.close();
         } catch (Exception e) {
             e.printStackTrace();
         }        
@@ -54,7 +90,7 @@ public class PearaDiSe {
         if (s.lastIndexOf("q") != -1) {
             return s.substring(s.lastIndexOf("q"));
         } else if (s.lastIndexOf("e") != -1) {
-            return s.substring(s.lastIndexOf("e"));            
+            return s.substring(s.lastIndexOf("e"));
         } else if (s.lastIndexOf("/") != -1) {
             return s.substring(s.lastIndexOf("/") + 1);
         } else {
